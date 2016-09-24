7b814bde2b66d297c205005d7341dc11158a0331
procsynth <mail@multoo.eu>
2016-09-24 16:45:49 +0200

    Added core files and folders

--- java/procsynth/shine3/Shine3.java
+++ java/procsynth/shine3/Shine3.java
@@ -7,13 +7,17 @@
 
 package procsynth.shine3;
 
+import java.net.URLClassLoader;
+import java.net.URL;
+import java.util.jar.Manifest;
+import java.util.jar.Attributes;
+import java.io.IOException;
 import processing.core.*;
 import processing.event.*;
 
 public class Shine3 extends PApplet{
 
-	public static String VERSION = "devel";
-
+	public static String VERSION = "unknow";
 	private static final String[] PAPPLET_ARGS = new String[] { "procsynth.shine3.Shine3" };
 
 	public static Shine3 m;
@@ -24,12 +28,12 @@ public class Shine3 extends PApplet{
 
 	public void settings() {
 		size(1280, 720, PApplet.P2D);
-		//noSmooth();
 	}
 
 	public void setup() {
+		VERSION = getVersion();
 		println("Shine3 "+ VERSION + " / procsynth");
-
+		
 		m = this;
 		frameRate(60);
 		surface.setResizable(true);
@@ -37,5 +41,18 @@ public class Shine3 extends PApplet{
 
 	public void draw() {
 		background(0x26, 0x32, 0x38); // BLUE_GREY._900
+	}
+
+	private String getVersion(){
+		String version = "unknow";
+		URLClassLoader cl = (URLClassLoader) getClass().getClassLoader();
+		try {
+			URL url = cl.findResource("META-INF/MANIFEST.MF");
+			Manifest manifest = new Manifest(url.openStream());
+			Attributes attr = manifest.getMainAttributes();
+			version = attr.getValue("Specification-Version");
+		} catch (IOException E) {
+		}
+		return version;
 	}
 }
